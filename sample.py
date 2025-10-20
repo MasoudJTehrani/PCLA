aa093d1db7195809436786cfeb0106e7b2eeefd9
MasoudJTehrani <masoudjt98@gmail.com>
2025-10-20 18:21:30 +0200

    changed default agent

--- sample.py
+++ sample.py
@@ -51,7 +51,7 @@ def main():
 
         world.tick()
 
-        agent = "lmdrive_llava"
+        agent = "tf_ltf"
         route = "./sample_route.xml"
         pcla = PCLA(agent, vehicle, route, client)
         
